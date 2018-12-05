3d60aabeb116c2ed7fa8561d728cbc62e2c5e7a4
LB519911 <liubiaozh@outlook.com>
2018-12-05 09:29:31 +0800

    ok

---
+++ JavaFxDemo/src/_4_grid_布局/Main1.java
@@ -0,0 +1,38 @@
+package _4_grid_布局;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main1 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //网格，左到右，上到下依次排序
+        GridPane gridPane=new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(60);
+        gridPane.setHgap(30);
+        gridPane.setPadding(new Insets(10,10,10,10));
+        final int column=3;
+        final int row=4;
+        for(int i=0;i!=column;i++){
+            for(int j=0;j!=row;j++){
+                gridPane.add(new Button("{row:" + j + ",column:" + i + "}"),i,j);
+            }
+        }
+
+        Scene scene=new Scene(gridPane);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(900);
+        primaryStage.setHeight(550);
+        primaryStage.show();
+    }
+}
---
+++ JavaFxDemo/src/_4_grid_布局/Main2.java
@@ -0,0 +1,40 @@
+package _4_grid_布局;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //网格，左到右，上到下依次排序
+        GridPane gridPane=new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(60);
+        gridPane.setHgap(30);
+        gridPane.setPadding(new Insets(10,10,10,10));
+        final int column=3;
+        final int row=4;
+        Button[][] btns=new Button[column][row];
+        btns[0][0]=new Button("{row:" + 0 + ",column:" + 0 + "}");
+        btns[0][0].setPrefSize(130,20);
+        gridPane.add(btns[0][0],0,0,2,1);
+        btns[2][0]=new Button("{row:" + 0 + ",column:" + 2 + "}");
+        btns[2][0].setPrefSize(60,20);
+        gridPane.add(btns[2][0],2,0);
+
+        Scene scene=new Scene(gridPane);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(900);
+        primaryStage.setHeight(550);
+        primaryStage.show();
+    }
+}
