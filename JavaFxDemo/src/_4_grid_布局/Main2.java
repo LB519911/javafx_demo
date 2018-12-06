7c9cb8f7a7da324fb291d32bd746126991b50a84
LB519911 <liubiaozh@outlook.com>
2018-12-06 11:33:17 +0800

    ok

--- JavaFxDemo/src/_4_grid_布局/Main2.java
+++ JavaFxDemo/src/_4_grid_布局/Main2.java
@@ -18,13 +18,43 @@ public class Main2 extends Application {
         //网格，左到右，上到下依次排序
         GridPane gridPane=new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setVgap(60);
-        gridPane.setHgap(30);
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
         gridPane.setPadding(new Insets(10,10,10,10));
         final int column=3;
         final int row=4;
         Button[][] btns=new Button[column][row];
+        btns[0][0] = new Button("0,0");
+        btns[0][0].setPrefSize(130,20);
+        gridPane.add(btns[0][0],0,0,2,1);
 
+        btns[2][0]=new Button("2,0");
+        btns[2][0].setPrefSize(60,20);
+        gridPane.add(btns[2][0],2,0);
+
+        btns[0][1]=new Button("0,1");
+        btns[0][1].setPrefSize(60,20);
+        gridPane.add(btns[0][1],0,1);
+
+        btns[1][1]=new Button("1,1");
+        btns[1][1].setPrefSize(130,20);
+        gridPane.add(btns[1][1],1,1,2,1);
+
+        btns[0][2]=new Button("0,2");
+        btns[0][2].setPrefSize(60,50);
+        gridPane.add(btns[0][2],0,2,1,2);
+
+        btns[1][2]=new Button("1,2");
+        btns[1][2].setPrefSize(60,20);
+        gridPane.add(btns[1][2],1,2);
+
+        btns[1][3]=new Button("1,3");
+        btns[1][3].setPrefSize(60,20);
+        gridPane.add(btns[1][3],1,3);
+
+        btns[2][2]=new Button("2,2");
+        btns[2][2].setPrefSize(60,50);
+        gridPane.add(btns[2][2],2,2,1,2);
 
         Scene scene=new Scene(gridPane);
         primaryStage.setScene(scene);
